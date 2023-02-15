aea776d996ce313ba2e5cb7dc9b7f9905d4ff54f
Aminu Rabiu <arabiu033@gmail.com>
2023-02-15 20:59:18 +0100

    Add actions required for the views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,34 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.all.where('user_id = ?', User.first)
+  end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @services = @category.all_services
+    @total_sum = @category.totalSum
+  end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def create; end
+  def create
+    @category = Category.create(category_params)
+    @category.user = User.first
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: "#{@category.name} was successfully created." }
+      else
+        flash[:error] = @category.errors.full_messages
+        format.html { redirect_to new_category_path }
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon, :description)
+  end
 end
