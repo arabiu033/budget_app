923c9a0f3cc4bb8e0c199e74386b6c74df022785
Aminu Rabiu <arabiu033@gmail.com>
2023-02-15 20:59:59 +0100

    Add some helping methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,13 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
   validates :icon, presence: true
+
+  def all_services
+    @services = ServiceCategory.includes(:service).where('category_id = ?', id).order(:updated_at)
+    @services
+  end
+
+  def total_sum
+    all_services.sum('amount')
+  end
 end
