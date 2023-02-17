8e52683a97d01f0a6dad6d99eed14f4505df5fe8
Aminu Rabiu <arabiu033@gmail.com>
2023-02-17 10:04:44 +0100

    style the devise pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable, :confirmable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :services, dependent: :destroy
   has_many :categories, dependent: :destroy
 
